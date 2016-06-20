3d4f77e050dd669302d17e3b60da67def59685a1
John Arban Lewis <johnarban@users.noreply.github.com>
2016-06-20 17:11:35 -0400

    massive update to amazingrace.py
    This will now work as a script run as `python amazingrace.py` and will present statistics at the end (so you could do it without people, but what fun would that be)

--- amazingrace.py
+++ amazingrace.py
@@ -1,13 +1,33 @@
+import sys
 import numpy as np
+
+python3 = sys.version_info.major == 3 # set a boolean indicating if we are in python 2 or 3
 
 dir = [-1,1]
 
-l = 1# enter some number
+l = 1 # enter some number
 
-def rndir():
-  return np.random.choice([-1,1])
+def get_keypress(ver3):
+  '''
+  Utility function so that getting a keypress
+  will work in either Python 2 or 3. 
+  '''
+  if not ver3:
+    return raw_input('Press [R] to Roll or [Q] to Quit [Default R]: ')
+  else:
+    return input('Press [R] to Roll or [Q] to Quit [Default R]: ')
 
-print l * rndir()
+roll = True
+rolls = []
+while roll:
+  rolls.append(l * np.random.choice(dir))
+  print(rolls[-1])
+  key = get_keypress(python3)
+  roll = not (key.lower() == 'q') # if user enters Q, exit. Otherwise new roll
+  
+print('\n ===== Statistics ======')
+print('Rolls: %i'%len(rolls))
+print('Sum: %i'%np.sum(rolls))
+print('\nHow much wood would a woodchuck chuck ...')
 
 
-print('How much wood could a woodchuck chuck ...')
